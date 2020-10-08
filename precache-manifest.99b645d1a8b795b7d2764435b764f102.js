294f5e0a8d648784d0c9f634061c9077a483824a
kamalelazizialaoui <elazizialaouikamal@yahoo.com>
2020-10-08 18:47:50 -0400

    Updates

---
+++ precache-manifest.99b645d1a8b795b7d2764435b764f102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344670f8d570acf71293bd094e8d1025",
+    "url": "/search_Developers/index.html"
+  },
+  {
+    "revision": "913066f11a1f6da3dc8a",
+    "url": "/search_Developers/static/css/main.51aac495.chunk.css"
+  },
+  {
+    "revision": "bab068e696cfcebe47e1",
+    "url": "/search_Developers/static/js/2.63c90ca0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/search_Developers/static/js/2.63c90ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913066f11a1f6da3dc8a",
+    "url": "/search_Developers/static/js/main.12eb020a.chunk.js"
+  },
+  {
+    "revision": "afa46547f18e72263432",
+    "url": "/search_Developers/static/js/runtime-main.067b4ec3.js"
+  }
+]);
